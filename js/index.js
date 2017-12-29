0d8525aab3aec9503b04ead6714549bfc2d606b4
elbert.kim@icloud.com <lkstandsfor@gmail.com>
2017-12-29 16:17:43 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var quiz = document.getElementById('quiz');
+
+quiz.style.cursor = 'pointer';
+quiz.onclick = function(){
+  window.location.href = "quiz_0.html";
+  //window.location.replace("quiz_0.html");
+}
+
+var hoz = document.getElementById('quiz');
+
+quiz.style.cursor = 'pointer';
+quiz.onclick = function(){
+  window.location.href = "https://suppzupp.myshopify.com";
+  //window.location.replace("quiz_0.html");
+}
---
+++ js/quiz_0.js
@@ -0,0 +1,6 @@
+var take_back = document.getElementById("take_back");
+take_back.style.cursor = 'pointer';
+
+take_back.onclick = function(){
+  window.location.href = "index.html";
+}
