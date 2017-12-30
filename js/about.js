de5cf35b5c8aba6a798f0c26d86760fd0d7042b1
elbert.kim@icloud.com <lkstandsfor@gmail.com>
2017-12-30 10:13:33 -0500

    update about.html

---
+++ js/about.js
@@ -0,0 +1,5 @@
+var go_back = document.getElementById('go_back');
+go_back.style.cursor = 'pointer';
+go_back.onclick = function(){
+  window.location.href = "index.html";
+}
--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 var quiz = document.getElementById('quiz');
-
 quiz.style.cursor = 'pointer';
 quiz.onclick = function(){
   window.location.href = "quiz_0.html";
@@ -7,7 +6,6 @@ quiz.onclick = function(){
 }
 
 var hoz = document.getElementById('hoz');
-
 hoz.style.cursor = 'pointer';
 hoz.onclick = function(){
   window.location.href = "https://suppzupp.myshopify.com/blogs/humans-of-zupp";
@@ -15,22 +13,13 @@ hoz.onclick = function(){
 }
 
 var market = document.getElementById('market');
-
 market.style.cursor = 'pointer';
 market.onclick = function(){
   window.location.href = "https://suppzupp.myshopify.com/search"
 }
 
 var about = document.getElementById('about');
-
 about.style.cursor = 'pointer';
 about.onclick = function(){
   window.location.href = "about.html"
-}
-
-var go_back = document.getElementById('go_back');
-
-go_back.style.cursor = 'pointer';
-go_back.onclick = function(){
-  window.location.href = "index.html";
 }
