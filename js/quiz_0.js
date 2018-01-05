c293396772a0d4d1493b07337cd982fd7e9396d6
elbert.kim@icloud.com <lkstandsfor@gmail.com>
2018-01-05 18:12:52 -0500

    start quiz

--- js/quiz_0.js
+++ js/quiz_0.js
@@ -1,3 +1,27 @@
+// var database;
+// var ref;
+// var data;
+// var sure;
+// var back;
+//
+// var config = {
+//   apiKey: "AIzaSyDV2A0nsbVX_bPyGpjH9tn4lYkWnYtqk_k",
+//   authDomain: "zupp-7c197.firebaseapp.com",
+//   databaseURL: "https://zupp-7c197.firebaseio.com",
+//   projectId: "zupp-7c197",
+//   storageBucket: "",
+//   messagingSenderId: "558183275118"
+// };
+// firebase.initializeApp(config);
+// database = firebase.database();
+// ref = database.ref('need_help')
+// data = {
+//   sure: true,
+//   back: false
+// }
+// ref.push(data);
+
+
 var take_back = document.getElementById("take_back");
 take_back.style.cursor = 'pointer';
 
